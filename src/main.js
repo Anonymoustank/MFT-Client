836c2621767ea2c86639516b4349979960aabba7
Anonymoustank <pkadekodi@gmail.com>
2024-04-12 12:00:12 -0400

    Fixed storage button styling

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ const path = require('node:path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    minWidth: 800,
+    minHeight: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
