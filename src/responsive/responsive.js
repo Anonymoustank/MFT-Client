5c9c013db7785a87c9e2a2177d55d447e4258107
Anonymoustank <pkadekodi@gmail.com>
2024-04-14 23:32:46 -0400

    Added file ui

--- src/responsive/responsive.js
+++ src/responsive/responsive.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
-    $(".squareBtn").each(function() {
+    $(".squareBtn, .fileBtn").each(function() {
         const lightPurple = "rgb(98, 93, 110)";
         const darkPurple = "rgb(59, 54, 70)";
+
+        $(this).attr("selected", false);
 
         $(this).on("click", function() {
             const currentColor = $(this).css("background-color")
             if (currentColor === lightPurple) {
                 $(this).css("background-color", darkPurple);
+                $(this).attr("selected", true);
             } else {
                 $(this).css("background-color", lightPurple);
+                $(this).attr("selected", false);
             }
         });
     });
+
 });
