6939f93e8556f866527b481f2434933464e6920f
Anonymoustank <pkadekodi@gmail.com>
2024-04-20 17:18:20 -0400

    Added some async/await code

--- src/responsive/responsive.js
+++ src/responsive/responsive.js
@@ -1,5 +1,26 @@
+async function getData() { //WORK IN PROGRESS - TODO
+    try {
+        const response = await fetch('http://localhost:5500/list-storages'); // Replace with the actual URL
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Data received:', data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
 $(document).ready(function() {
-    $(".squareBtn, .fileBtn").each(function() {
+
+    getData();
+
+    var hello = "Click me!";
+
+    // Use jQuery to append a button with the text from the "hello" variable
+    $("#mainStorageLocation").append(`<button class='squareBtn'>${hello}</button>`);
+
+    $(".squareBtn, .fileBtn").each(function() { //NEEDS TO BE AFTER BUTTONS ARE CREATED
         const lightPurple = "rgb(98, 93, 110)";
         const darkPurple = "rgb(59, 54, 70)";
 
