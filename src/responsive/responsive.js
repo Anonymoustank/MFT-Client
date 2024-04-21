df13f17a7c6610e32667f6af316abd8cfa24fc85
Anonymoustank <pkadekodi@gmail.com>
2024-04-20 21:33:35 -0400

    Populates storage list with data from backend

--- src/responsive/responsive.js
+++ src/responsive/responsive.js
@@ -1,11 +1,11 @@
-async function getData() { //WORK IN PROGRESS - TODO
+async function getData() {
     try {
-        const response = await fetch('http://localhost:5500/list-storages'); // Replace with the actual URL
+        const response = await fetch('http://localhost:5500/list-storages'); //get data from backend
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
-        console.log('Data received:', data);
+        return data;
     } catch (error) {
         console.error('Error fetching data:', error);
     }
@@ -13,29 +13,29 @@ async function getData() { //WORK IN PROGRESS - TODO
 
 $(document).ready(function() {
 
-    getData();
+    getData().then(result => {
+        for (let i = 0; i < result["storageList"].length; i++) {
+            const storageName = result["storageList"][i].storageName
+            $("#mainStorageLocation").append(`<button class='squareBtn'>${storageName}</button>`); //create buttons with storage names
+        }
 
-    var hello = "Click me!";
-
-    // Use jQuery to append a button with the text from the "hello" variable
-    $("#mainStorageLocation").append(`<button class='squareBtn'>${hello}</button>`);
-
-    $(".squareBtn, .fileBtn").each(function() { //NEEDS TO BE AFTER BUTTONS ARE CREATED
-        const lightPurple = "rgb(98, 93, 110)";
-        const darkPurple = "rgb(59, 54, 70)";
-
-        $(this).attr("selected", false);
-
-        $(this).on("click", function() {
-            const currentColor = $(this).css("background-color")
-            if (currentColor === lightPurple) {
-                $(this).css("background-color", darkPurple);
-                $(this).attr("selected", true);
-            } else {
-                $(this).css("background-color", lightPurple);
-                $(this).attr("selected", false);
-            }
+        $(".squareBtn, .fileBtn").each(function() { //make buttons toggle-able
+            const lightPurple = "rgb(98, 93, 110)";
+            const darkPurple = "rgb(59, 54, 70)";
+    
+            $(this).attr("selected", false);
+    
+            $(this).on("click", function() {
+                const currentColor = $(this).css("background-color")
+                if (currentColor === lightPurple) {
+                    $(this).css("background-color", darkPurple);
+                    $(this).attr("selected", true);
+                } else {
+                    $(this).css("background-color", lightPurple);
+                    $(this).attr("selected", false);
+                }
+            });
         });
-    });
+    })
 
 });
